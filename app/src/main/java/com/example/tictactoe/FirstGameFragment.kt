a35a4912d490cfdec3a3e81b7963134846aabcbe
volkova-lisa <liza.volk999@gmail.com>
2021-10-13 20:50:00 +0300

    fixed lots of bugs

--- app/src/main/java/com/example/tictactoe/FirstGameFragment.kt
+++ app/src/main/java/com/example/tictactoe/FirstGameFragment.kt
@@ -9,41 +9,34 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.content.ContextCompat
-import androidx.navigation.Navigation
+import androidx.core.content.res.ResourcesCompat
+import com.example.tictactoe.databinding.FragmentFirstGameBinding
 import com.example.tictactoe.databinding.FragmentStartBinding
 
 class FirstGameFragment : Fragment() {
 
     lateinit var gameManager: MyGameManager
-    private lateinit var one: TextView
-    private lateinit var two: TextView
-    private lateinit var three: TextView
-    private lateinit var four: TextView
-    private lateinit var five: TextView
-    private lateinit var six: TextView
-    private lateinit var seven: TextView
-    private lateinit var eight: TextView
-    private lateinit var nine: TextView
 
-    lateinit var gameButton: Button
-    private var _binding : FragmentStartBinding? = null
+    private var _binding : FragmentFirstGameBinding? = null
     val mBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentStartBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentFirstGameBinding.inflate(layoutInflater, container, false)
 
-        one.setOnClickListener { onBoxClicked(one, Position(0, 0)) }
-        two.setOnClickListener { onBoxClicked(two, Position(0, 1)) }
-        three.setOnClickListener { onBoxClicked(three, Position(0, 2)) }
-        four.setOnClickListener { onBoxClicked(four, Position(1, 0)) }
-        five.setOnClickListener { onBoxClicked(five, Position(1, 1)) }
-        six.setOnClickListener { onBoxClicked(six, Position(1, 2)) }
-        seven.setOnClickListener { onBoxClicked(seven, Position(2, 0)) }
-        eight.setOnClickListener { onBoxClicked(eight, Position(2, 1)) }
-        nine.setOnClickListener { onBoxClicked(nine, Position(2, 2)) }
+        gameManager = MyGameManager()
+
+        mBinding.one.setOnClickListener { onBoxClicked(mBinding.one, Position(0, 0)) }
+        mBinding.two.setOnClickListener { onBoxClicked(mBinding.two, Position(0, 1)) }
+        mBinding.three.setOnClickListener { onBoxClicked(mBinding.three, Position(0, 2)) }
+        mBinding.four.setOnClickListener { onBoxClicked(mBinding.four, Position(1, 0)) }
+        mBinding.five.setOnClickListener { onBoxClicked(mBinding.five, Position(1, 1)) }
+        mBinding.six.setOnClickListener { onBoxClicked(mBinding.six, Position(1, 2)) }
+        mBinding.seven.setOnClickListener { onBoxClicked(mBinding.seven, Position(2, 0)) }
+        mBinding.eight.setOnClickListener { onBoxClicked(mBinding.eight, Position(2, 1)) }
+        mBinding.nine.setOnClickListener { onBoxClicked(mBinding.nine, Position(2, 2)) }
 
         return mBinding.root
     }
@@ -54,12 +47,12 @@ class FirstGameFragment : Fragment() {
             val winningLine = gameManager.makeMove(position)
             if (winningLine != null) {
                 disableBoxes()
-                gameButton.visibility = View.VISIBLE
+                mBinding.startNewGameButton.visibility = View.VISIBLE
                 showWinner(winningLine)
             }
         }
-        gameButton.setOnClickListener {
-            gameButton.visibility = View.GONE
+        mBinding.startNewGameButton.setOnClickListener {
+            mBinding.startNewGameButton.visibility = View.GONE
             gameManager.reset()
             resetboxes()
         }
@@ -67,66 +60,66 @@ class FirstGameFragment : Fragment() {
     }
 
     private fun resetboxes() {
-        one.text = ""
-        two.text = ""
-        three.text = ""
-        four.text = ""
-        five.text = ""
-        six.text = ""
-        seven.text = ""
-        eight.text = ""
-        nine.text = ""
-        one.background = null
-        two.background = null
-        three.background = null
-        four.background = null
-        five.background = null
-        six.background = null
-        seven.background = null
-        eight.background = null
-        nine.background = null
-        one.isEnabled = true
-        two.isEnabled = true
-        three.isEnabled = true
-        four.isEnabled = true
-        five.isEnabled = true
-        six.isEnabled = true
-        seven.isEnabled = true
-        eight.isEnabled = true
-        nine.isEnabled = true
+        mBinding.one.text = ""
+        mBinding.two.text = ""
+        mBinding.three.text = ""
+        mBinding.four.text = ""
+        mBinding.five.text = ""
+        mBinding.six.text = ""
+        mBinding.seven.text = ""
+        mBinding.eight.text = ""
+        mBinding.nine.text = ""
+        mBinding.one.background = null
+        mBinding.two.background = null
+        mBinding.three.background = null
+        mBinding.four.background = null
+        mBinding.five.background = null
+        mBinding.six.background = null
+        mBinding.seven.background = null
+        mBinding.eight.background = null
+        mBinding.nine.background = null
+        mBinding.one.isEnabled = true
+        mBinding.two.isEnabled = true
+        mBinding.three.isEnabled = true
+        mBinding.four.isEnabled = true
+        mBinding.five.isEnabled = true
+        mBinding.six.isEnabled = true
+        mBinding.seven.isEnabled = true
+        mBinding.eight.isEnabled = true
+        mBinding.nine.isEnabled = true
     }
 
 
     private fun disableBoxes() {
-        one.isEnabled = false
-        two.isEnabled = false
-        three.isEnabled = false
-        four.isEnabled = false
-        five.isEnabled = false
-        six.isEnabled = false
-        seven.isEnabled = false
-        eight.isEnabled = false
-        nine.isEnabled = false
+        mBinding.one.isEnabled = false
+        mBinding.two.isEnabled = false
+        mBinding.three.isEnabled = false
+        mBinding.four.isEnabled = false
+        mBinding.five.isEnabled = false
+        mBinding.six.isEnabled = false
+        mBinding.seven.isEnabled = false
+        mBinding.eight.isEnabled = false
+        mBinding.nine.isEnabled = false
     }
 
     private fun showWinner(winningLine: WinningLine) {
         val (winningBoxes, background) = when (winningLine) {
-            WinningLine.ROW_0 -> Pair(listOf(one, two, three), R.drawable.horiz_line)
-            WinningLine.ROW_1 -> Pair(listOf(four, five, six), R.drawable.horiz_line)
-            WinningLine.ROW_2 -> Pair(listOf(seven, eight, nine), R.drawable.horiz_line)
-            WinningLine.COLUMN_0 -> Pair(listOf(one, four, seven), R.drawable.vert_line)
-            WinningLine.COLUMN_1 -> Pair(listOf(two, five, eight), R.drawable.vert_line)
-            WinningLine.COLUMN_2 -> Pair(listOf(three, six, nine), R.drawable.vert_line)
-            WinningLine.DIAGONAL_LEFT -> Pair(listOf(one, five, nine),
+            WinningLine.ROW_0 -> Pair(listOf(mBinding.one, mBinding.two, mBinding.three), R.drawable.horiz_line)
+            WinningLine.ROW_1 -> Pair(listOf(mBinding.four, mBinding.five, mBinding.six), R.drawable.horiz_line)
+            WinningLine.ROW_2 -> Pair(listOf(mBinding.seven, mBinding.eight, mBinding.nine), R.drawable.horiz_line)
+            WinningLine.COLUMN_0 -> Pair(listOf(mBinding.one, mBinding.four, mBinding.seven), R.drawable.vert_line)
+            WinningLine.COLUMN_1 -> Pair(listOf(mBinding.two, mBinding.five, mBinding.eight), R.drawable.vert_line)
+            WinningLine.COLUMN_2 -> Pair(listOf(mBinding.three, mBinding.six, mBinding.nine), R.drawable.vert_line)
+            WinningLine.DIAGONAL_LEFT -> Pair(listOf(mBinding.one, mBinding.five, mBinding.nine),
                 R.drawable.left_digonal
             )
-            WinningLine.DIAGONAL_RIGHT -> Pair(listOf(three, five, seven),
+            WinningLine.DIAGONAL_RIGHT -> Pair(listOf(mBinding.three, mBinding.five, mBinding.seven),
                 R.drawable.right_diagonal
             )
         }
 
         winningBoxes.forEach { box ->
-            box.background = ContextCompat.getDrawable(background)
+            box.background = ResourcesCompat.getDrawable(resources, R.drawable.right_diagonal, null)
         }
     }
 
